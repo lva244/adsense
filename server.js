f8031af9b9f6388c4b16b0de6587260bc8ab7018
lva244 <lvanh24494@gmail.com>
2016-02-02 23:54:26 +0700

    update

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var mongoose = require('./config/mongoose'),
 	 express = require('./config/express');
 	
-
 var db = mongoose();
 var app = express(db);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 module.exports = app;
 
 console.log("Server running at http://localhost:3000/");
